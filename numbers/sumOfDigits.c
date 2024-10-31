2e324caa11f62f295c4171108d15c7018ba8dbd2
Hasan <9131411681hasan@gmail.com>
2024-10-31 21:03:11 +0530

    minor refactor

--- numbers/sumOfDigits.c
+++ numbers/sumOfDigits.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-int sumOfDigits();
+int sumOfDigits(int input)
+{
+    int sum = 0;
+    int remaining = input;
 
-int main() {
-    int n = 321;
-    int sumNum = sumOfDigits(n);
-    printf("Reverse of %d is: %d",n, sumNum);
-
-}
-int sumOfDigits(int input) {
-    int sum = 0;  
-    int remaining = input;  
-
-    while (remaining > 0) {
-        int unitPlace = remaining % 10;     
-        sum = sum + unitPlace; 
-        remaining /= 10;                    
+    while (remaining > 0)
+    {
+        int unitPlace = remaining % 10;
+        sum = sum + unitPlace;
+        remaining /= 10;
     }
 
     return sum;
+}
+
+int main()
+{
+    int n = 321;
+    int sumNum = sumOfDigits(n);
+    printf("Reverse of %d is: %d", n, sumNum);
 }
